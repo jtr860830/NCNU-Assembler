0944a145c6ffdf7b0c83150aaa75e6a403a98920
Josh Hsieh <dev@josh-hsieh.tw>
2024-04-12 15:03:19 +0800

    feat: add symbol table implementation in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,76 @@ impl SymbolTable {
         }
     }
 
+    pub fn insert(&mut self, label: String, address: Address) -> Result<(), String> {
+        if !SymbolTable::is_label_legal(&label) {
+            return Err("Illegal label".to_string());
+        }
+
+        if self.is_label_exists(&label) {
+            return Err("Label already exists".to_string());
+        }
+
+        self.table.insert(label, Symbol {
+            address,
+            waiting_list: Vec::new(),
+        });
+
+        Ok(())
+    }
+
+    pub fn find(&self, label: &str) -> Option<&Symbol> {
+        self.table.get(label)
+    }
+
+    pub fn push_waiting_list(&mut self, label: &str, address: u32) -> Result<(), String> {
+        if !self.is_label_exists(label) {
+            return Err("Label does not exist".to_string());
+        }
+
+        let symbol = self.table.get_mut(label).unwrap();
+        match symbol.address {
+            Address::Unallocated => {
+                symbol.waiting_list.push(address);
+            },
+            Address::Allocated(_) => {
+                return Err("Label already allocated".to_string());
+            },
+        }
+
+        Ok(())
+    }
+
+    pub fn allocate(&mut self, label: &str, address: u32) -> Result<(), String> {
+        if !self.is_label_exists(label) {
+            return Err("Label does not exist".to_string());
+        }
+
+        let symbol = self.table.get_mut(label).unwrap();
+        match symbol.address {
+            Address::Unallocated => {
+                symbol.address = Address::Allocated(address);
+            },
+            Address::Allocated(_) => {
+                return Err("Label already allocated".to_string());
+            },
+        }
+
+        Ok(())
+    }
+
+    pub fn print(&self) {
+        for (label, symbol) in &self.table {
+            match symbol.address {
+                Address::Unallocated => println!("{}: Unallocated", label),
+                Address::Allocated(addr) => println!("{}: Allocated at {}", label, addr),
+            }
+        }
+    }
+
+    pub fn is_label_exists(&self, label: &str) -> bool {
+        self.table.contains_key(label)
+    }
+
     pub fn is_label_legal(label: &str) -> bool {
         // Check if the label is empty
         if label.is_empty() {
@@ -40,30 +110,5 @@ impl SymbolTable {
         }
         
         true
-    }
-
-    pub fn insert(&mut self, label: String, address: Address) {
-        todo!("Implement insert");
-    }
-
-    pub fn remove(&mut self, label: &str) {
-        todo!("Implement remove");
-    }
-
-    pub fn find(&self, label: &str) {
-        todo!("Implement find");
-    }
-
-    pub fn update(&mut self, label: &str, address: Address) {
-        todo!("Implement update");
-    }
-
-    pub fn print(&self) {
-        for (label, symbol) in &self.table {
-            match symbol.address {
-                Address::Unallocated => println!("{}: Unallocated", label),
-                Address::Allocated(addr) => println!("{}: Allocated at {}", label, addr),
-            }
-        }
     }
 }
